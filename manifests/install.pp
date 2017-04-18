7810016b6a276bd6c1e86d0538ca53383a0699e7
Paul O'Connor <paul@poconnor.me>
2017-04-18 18:00:13 +0100

    Update install.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -25,7 +25,7 @@ class codedeploy::install {
         }
       }
       exec { 'download_codedeploy_installer':
-        command => '/usr/local/bin/aws s3 cp http://us-east-1.s3.amazonaws.com/latest/install . --region us-east-1',
+        command => '/usr/local/bin/aws s3 cp http://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install . --region us-east-1',
         cwd     => '/tmp',
         creates => '/tmp/install',
       }
